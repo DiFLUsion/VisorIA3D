fb7f7e5ba5c0765e3de8675ebc0688bd7644e157
influenzaAviar <78164319+influenzaAviar@users.noreply.github.com>
2022-03-04 17:18:42 +0100

    opacidad rojo brotes
    opacidad rojo brotes

--- js/aviarScript.js
+++ js/aviarScript.js
@@ -41,7 +41,7 @@ require([
           //   primitive: "square"
           // },
           material: {
-            color: [255, 0, 0, 0.3]
+            color: [255, 0, 0, 0.4]
           }
         }
       ]
